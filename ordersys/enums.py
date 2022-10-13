29c55ab61af264bdbcbbd92a6e3d278513101246
Arsh Noor Amin <ana002@bucknell.edu>
2022-10-13 11:41:20 -0400

    "added new order functionality"

---
+++ ordersys/enums.py
@@ -0,0 +1,26 @@
+vendors = {
+    "Digi-Key": "https://www.digikey.com/",
+    "Mouser": "https://www.mouser.com/",
+    "McMaster-Carr": "https://www.mcmaster.com/",
+    "Amazon": "https://www.amazon.com/",
+    "Sparkfun": "https://www.sparkfun.com/",
+    "Pololu": "https://www.pololu.com/",
+    "Adafruit" : "https://www.adafruit.com/",
+    "PCBWay": "https://www.pcbway.com/",
+    "OSH Park": "https://oshpark.com/",
+    "Advanced Circuits": "https://www.4pcb.com/",
+    "OSH Stencils": "https://www.oshstencils.com/",
+    "Makerstock": "https://stock.maker.bucknell.edu/",
+    "Other": "NA"
+}
+
+status_enum = {
+    0: "Pending",
+    1: "Approved",
+    2: "Ordered",
+    3: "Shipped",
+    4: "Delivered",
+    5: "Canceled"
+}
+
+
